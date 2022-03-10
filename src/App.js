2b7a3dff43346a0ed2fe4505f7133b018a1299c1
agoudoussy <goudoussy2000@gmail.com>
2022-03-10 14:19:25 +0000

    Add project structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <div>
+     <Home/>
     </div>
   );
 }
