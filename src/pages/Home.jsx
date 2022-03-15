0fe344877314b8a12ed9091b97616fdb736e1a25
agoudoussy <goudoussy2000@gmail.com>
2022-03-15 11:41:58 +0000

    home done half

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,18 @@ import "./home.css"
 function Home() {
   return (
     <div className='home_container full-page '>
-      
+      <div className='left-container'>
+        <h5> SO, YOU WANT TO TRAVEL</h5>
+        <h1>SPACE</h1>
+        <p> Let’s face it; if you want to go to space, you might as well genuinely go to 
+  outer space and not hover kind of on the edge of it. Well sit back, and relax 
+  because we’ll give you a truly out of this world experience!</p>
+      </div>
+      <div className='right-container'>
+        <div className='main-button'>
+        <h4>EXPLORE</h4>
+        </div>
+      </div>
     </div>
   )
 }
